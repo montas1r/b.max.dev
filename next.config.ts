42e277bb72070af14c1127c10f78c4bdadc44464
Nothing Here <nhere796@gmail.com>
2026-03-07 16:25:11 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (https://irisrea

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'irisreading.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
